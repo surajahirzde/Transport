5c1bfcfb579ad0cd75aa46af5a22896960547629
surajahirzde <sy241527@gmail.com>
2025-12-05 12:25:07 +0530

    ABC

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../Helper/Navbar';
 import Hero from '../Components/Hero'; 
 import './styles/Home.css';
-import Features from '../components/Additional/Feature'; 
+import Features from '../Components/Additional/Feature'; 
 
 const Home = () => {
   return (
